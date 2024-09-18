dec82ac0f9a00c351e280238c52c8ab9bc597328
luqi <lq.xie.work@gmail.com>
2024-09-17 18:15:26 -0700

    finish with both for and while loop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 SUITS = ["♥", "♦", "♠", "♦︎"]
 
@@ -6,4 +7,24 @@ deck_of_cards = [f"{card}{suit}" for card in CARDS for suit in SUITS]
 # Implement a solution for shuffling a deck of cards without using random.shuffle()
 # Using randint() is ok
 def shuffle_deck(deck_of_cards):
-    pass
+    shuffled_deck = []
+    
+    # while len(shuffled_deck) < len(deck_of_cards):
+    #     index = random.randint(0, len(deck_of_cards)-1)
+    #     if not deck_of_cards[index] in shuffled_deck:
+    #         shuffled_deck.append(deck_of_cards[index])
+    
+    copy_of_deck_of_cards = list(deck_of_cards)
+    for _ in range(0, len(deck_of_cards)):
+        index = random.randint(0, len(copy_of_deck_of_cards)-1)
+        shuffled_deck.append(copy_of_deck_of_cards[index])
+        copy_of_deck_of_cards.pop(index)
+
+
+    return shuffled_deck
+
+
+
+shuffled_deck = shuffle_deck(deck_of_cards)
+print(shuffled_deck)
+print(len(shuffled_deck))
